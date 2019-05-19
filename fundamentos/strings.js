6b77a4d2d9506846240eabab360c8e0bae901eca
Robson Quaresma <robquaresma@gmail.com>
2019-05-19 09:40:34 -0300

    Strings

---
+++ fundamentos/strings.js
@@ -0,0 +1,16 @@
+const escola = 'Cod3r';
+
+console.log(escola.charAt(4)); // retorna o valor no índice informado
+console.log(escola.charAt(5)); // não retorna erro
+console.log(escola.charCodeAt(3)); // retorna o valor na tabela ASC
+console.log(escola.indexOf('3')); // valida se há o valor passado na variável fornecida, retornando o índice onde econtra-se o valor
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+console.log('Escola '.concat(escola).concat('!'));
+console.log(escola.replace(3, 'e'));
+console.log('Ana,Maria,Pedro'.split(',')); // gera um array
+
+
+
+
+
