a478e408c78044000e5682c60728d694c2235460
Robson Quaresma <robquaresma@gmail.com>
2019-06-20 19:41:35 -0300

    Função: factory 2

---
+++ funcao/factory2.js
@@ -0,0 +1,11 @@
+
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Notebook', 2299.99));
+console.log(criarProduto('iPad', 1299.99));
