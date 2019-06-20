0308442121f26cc95d920cb6ce94c68397a6c0a2
Robson Quaresma <robquaresma@gmail.com>
2019-06-20 19:31:04 -0300

    Função: factory 1

--- funcao/factory1.js
+++ funcao/factory1.js
@@ -1,1 +1,12 @@
 // Factory é uma função que devolve um objeto
+// Ou seja, uma fabrica de objetos
+
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa());
