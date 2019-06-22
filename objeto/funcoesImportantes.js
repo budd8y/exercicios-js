c752fe47c3c3792c2a1718bea92a8083ff182ffb
Robson Quaresma <robquaresma@gmail.com>
2019-06-22 18:02:47 -0300

    Função importantes de objetos

--- objeto/funcoesImportantes.js
+++ objeto/funcoesImportantes.js
@@ -15,3 +15,22 @@ console.log('----------------------');
 Object.entries(pessoa).forEach(([chave, valor]) => {
     console.log(`${chave}: ${valor}`);
 });
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true, // será exibido no keys
+    writable: false, // não deixa ser alterado (tipo um freeze)
+    value: '01/01/2019'
+});
+
+pessoa.dataNascimento = '01/01/2017';
+console.log(pessoa.dataNascimento);
+console.log(Object.keys(pessoa));
+
+// EC6
+const dest = { a: 1 };
+const o1 = { b: 2 };
+const o2 = { c: 3, a: 4 };
+const obj = Object.assign(dest, o1, o2);
+Object.freeze(obj);
+obj.c = 1234;
+console.log(obj);
