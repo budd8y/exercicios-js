ab0bf9d3fe0cbe884a6b6cbf68f38cc775afede8
Robson Quaresma <robquaresma@gmail.com>
2019-06-23 11:10:03 -0300

    Foreach 1

---
+++ array/foreach1.js
@@ -0,0 +1,16 @@
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach((nome, indice) => {
+    console.log(`${indice + 1}) ${nome}`)  
+})
+
+aprovados.forEach((nome, indice, array) => {
+    console.log(`${indice + 1}) ${nome}`)
+    console.log(array)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+
+aprovados.forEach(exibirAprovados)
