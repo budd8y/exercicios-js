ec6716d6e0618a82c35600c92f1447bd7d891d6b
Robson Quaresma <robquaresma@gmail.com>
2019-07-26 20:43:32 -0300

    Express - Aula 2

--- i_node/projeto/src/servidor.js
+++ i_node/projeto/src/servidor.js
@@ -2,14 +2,27 @@ const porta = 3004
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
     console.log('Middleware 1...')
     next()
 })
 
-app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // converte para JSON automaticamente
+app.get('/produtos', (req, res) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
