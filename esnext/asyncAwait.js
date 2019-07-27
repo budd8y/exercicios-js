c1d5038247a9c1d97c07d175b93571197273f34c
Robson Quaresma <robquaresma@gmail.com>
2019-07-27 16:41:47 -0300

    Async/Await para melhorar o uso de promises

---
+++ esnext/asyncAwait.js
@@ -0,0 +1,36 @@
+// com promise ...
+const http = require('http')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra.toUpperCase()}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+    
+            res.on('data', dados => {
+                resultado += dados
+            })
+    
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+// Recurso do ES8
+// Objetivo de simplificar o uso de promises...
+let obterAlunos = async () => {
+    const ta = await getTurma('a')
+    const tb = await getTurma('b')
+    const tc = await getTurma('c')
+    return [].concat(ta, tb, tc)
+} // retorna um objeto AsyncFunction
+
+obterAlunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then(nomes => console.log(nomes))
